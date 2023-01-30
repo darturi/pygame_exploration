ef0c66ef9c5606dabdcb4467633a7371e1d9f457
danielarturi <dan.arturi@gmail.com>
2023-01-29 23:15:29 -0500

    marker for when to switch velocites established

--- one_sprite.py
+++ one_sprite.py
@@ -11,6 +11,7 @@ WHITE = (255, 255, 255)
 FPS = 60
 VEL = 5
 SPRITE_W, SPRITE_H = 40, 40
+START_X, START_Y = 400, 200
 
 
 def create_arrow_sprite(w=SPRITE_W, h=SPRITE_H):
@@ -25,19 +26,62 @@ def rotate_sprite(sprite_name, angle, right=True):
     return pygame.transform.rotate(sprite_name, angle)
 
 
-def draw_window(arrow_loc):
+def determine_velocity(arrow_loc, arrow_prev_state, prev_x_velocity, prev_y_velocity, padding=50):
+    print("arrow_prev_state", arrow_prev_state, "   in_outer_padding(arrow_loc)", in_outer_padding(arrow_loc), "   in_inner_padding(arrow_loc)", in_inner_padding(arrow_loc))
+    new_x, new_y = prev_x_velocity, prev_y_velocity
+    if not arrow_prev_state and in_outer_padding(arrow_loc):
+        if arrow_loc <= padding or arrow_loc >= WIDTH - padding:
+            new_x *= -1
+        else:
+            new_y *= -1
+    return new_x, new_y
+
+
+def draw_window(arrow_loc, arrow_prev_state):
     WIN.fill(WHITE)
     WIN.blit(arrow, (arrow_loc.x, arrow_loc.y))
+
+    """if not arrow_prev_state and not in_outer_padding(arrow_loc):
+        update_loc(arrow_loc, arrow_prev_state, -1, -1)
+    else:
+        update_loc(arrow_loc, arrow_prev_state)"""
+    update_loc(arrow_loc, arrow_prev_state)
+
     pygame.display.update()
 
 
-def update_loc(arrow_loc, velocity=VEL):
-    arrow_loc.x += velocity
-    arrow_loc.y += velocity
+def in_outer_padding(arrow_loc, padding=50):
+    # check if in outer padding
+    if arrow_loc.x <= padding or arrow_loc.x >= WIDTH - padding or \
+            arrow_loc.y <= padding or arrow_loc.y >= HEIGHT - padding:
+        return True
+    return False
+
+
+def in_inner_padding(arrow_loc, padding=50, thickness=10):
+    return not in_outer_padding(arrow_loc) and in_outer_padding(arrow_loc, padding + thickness)
+
+
+def update_loc(arrow_loc, prev_state, x_vel=VEL, y_vel=VEL, threshold=10):
+    """# check if x flipping is needed
+    if arrow_loc.x == threshold or arrow_loc.x == WIDTH - threshold:
+        x_velocity *= -1
+    # check if y flipping is needed
+    if arrow_loc.y == threshold or arrow_loc.y == HEIGHT - threshold:
+        y_velocity *= -1"""
+
+    x_vel, y_vel = determine_velocity(arrow_loc, prev_state, x_vel, y_vel)
+
+    arrow_loc.x += x_vel
+    arrow_loc.y += y_vel
 
 
 def main():
-    arrow_loc = pygame.Rect(400, 200, SPRITE_W, SPRITE_H)
+    # define arrow start position
+    arrow_loc = pygame.Rect(START_X, START_Y, SPRITE_W, SPRITE_H)
+
+    # define initial arrow state
+    arrow_prev_state = in_outer_padding(arrow_loc)
 
     clock = pygame.time.Clock()
     run = True
@@ -47,9 +91,12 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
-        update_loc(arrow_loc)
+        draw_window(arrow_loc, arrow_prev_state)
 
-        draw_window(arrow_loc)
+        # update previous arrow position
+        arrow_prev_state = in_outer_padding(arrow_loc)
+
+        input()
 
     pygame.quit()
 
